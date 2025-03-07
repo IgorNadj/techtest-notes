94ae90d81574cd3f56ad4a9d76f28d3abc5c70c9
Igor Nadj <igor.nadj@gmail.com>
2025-03-07 17:02:05 +1100

    Extract new note button into own component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,34 @@
-import { SafeAreaView, View } from 'react-native';
-import { ListItem, VStack, Text } from '@react-native-material/core';
-import { NewNoteButton } from '../../components/NewNoteButton';
+import {SafeAreaView, View} from 'react-native';
+import {ListItem, VStack, Text} from '@react-native-material/core';
+import {NewNoteButton} from '../../components/NewNoteButton';
 
 
 const FAKE_DATA = [
-  { id: 1, note: 'This is a note' },
-  { id: 2, note: 'This is a note' },
-  { id: 3, note: 'This is a note' },
-  { id: 4, note: 'This is a note' },
-  { id: 5, note: 'This is a note' },
-  { id: 6, note: 'This is a note' }, 
+    {id: 1, note: 'This is a note'},
+    {id: 2, note: 'This is a note'},
+    {id: 3, note: 'This is a note'},
+    {id: 4, note: 'This is a note'},
+    {id: 5, note: 'This is a note'},
+    {id: 6, note: 'This is a note'},
 ]
 
 const onNewNoteStart = () => {
-  console.log('pressed');
+    console.log('pressed');
 }
 
 export default function HomeScreen() {
-  return (
-    <SafeAreaView>
-      <View>
-        <Text variant="h6" style={{ margin: 20 }}>Notes Test App</Text>
-        <NewNoteButton onPress={onNewNoteStart} />
-        <VStack>
-          <>
-            {FAKE_DATA.map((item) => <ListItem key={item.id} title={item.note} />)}
-          </>
-        </VStack>
-      </View>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView>
+            <View>
+                <Text variant="h6" style={{margin: 20}}>Notes Test App</Text>
+                <NewNoteButton onPress={onNewNoteStart}/>
+                <VStack>
+                    <>
+                        {FAKE_DATA.map((item) => <ListItem key={item.id} title={item.note}/>)}
+                    </>
+                </VStack>
+            </View>
+        </SafeAreaView>
+    );
 }
 
--- components/NewNoteButton.tsx
+++ components/NewNoteButton.tsx
@@ -1,18 +1,19 @@
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
-import { FAB, Box } from '@react-native-material/core';
+import {FAB, Box} from '@react-native-material/core';
 
 
 type NewNoteButtonProps = {
     onPress: () => void;
 }
 
-export const NewNoteButton = ({onPress }: NewNoteButtonProps ) => 
-        <Box style={{
-            position: 'absolute',
-            top: 20,
-            right: 20,
-            zIndex: 1000,
-        }}>
-            <FAB onPress={(e) => { onPress() }} color="primary" icon={props => <Icon name="plus" {...props} />} />
-        </Box>    
-    
+export const NewNoteButton = ({onPress}: NewNoteButtonProps) =>
+    <Box style={{
+        position: 'absolute',
+        top: 20,
+        right: 20,
+        zIndex: 1000,
+    }}>
+        <FAB onPress={(e) => {
+            onPress()
+        }} color="primary" icon={props => <Icon name="plus" {...props} />}/>
+    </Box>
